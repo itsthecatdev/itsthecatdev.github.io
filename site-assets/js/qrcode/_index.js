dec7bd24c150f5bdd4015cb0c27009a8d7509234
itsthecatdev <cutekitten03.cutekittenstudios@gmail.com>
2024-12-01 20:04:48 +0100

    24-w48-A

---
+++ site-assets/js/qrcode/_index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const qrCodeContainer = document.getElementById('qr-code');
+  
+    const url = 'https://itsthecatdev.github.io/projects/tastschreibgenie/me';
+    const qrCodeSize = 250; // Size in pixels
+  
+    // Use QRCode.js to generate the QR code
+    new QRCode(qrCodeContainer, {
+      text: url,
+      width: qrCodeSize,
+      height: qrCodeSize,
+    });
+  });
+  
